c7a118272a05a6e86e3992a7ec3fcedc7881da16
Tom Power <tp21@tuta.io>
2024-05-26 09:41:51 +0100

    fix linting

--- lib/mob/argsValues.sh
+++ lib/mob/argsValues.sh
@@ -1,12 +1,14 @@
+#!/bin/sh
+
 _jiraTicketsCurrent() {
   jira issue list \
     --label "Checkout_Team" -q"type in ('Sub-task', Story) and status in ('In Progress')"  \
     --plain --no-headers --columns key,summary
 }
 
-local -a tickets ticketsKeysDescriptions ticketsKeys
 _branchArgValuesJira() {
-  local tickets=(${(f)"$(_jiraTicketsCurrent)"})
+  local -a tickets ticketsKeysDescriptions ticketsKeys
+  local tickets=("${(f)"$(_jiraTicketsCurrent)"}")
   for ticket in "${tickets[@]}"; do
      ticketsKeysDescriptions+=$(echo "${ticket/$(echo -e '\t')/ -- }")
      ticketsKeys+=$(echo "$ticket" | cut -d$'\t' -f 1)
